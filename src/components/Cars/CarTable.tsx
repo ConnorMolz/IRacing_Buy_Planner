96d77150b8b95c8ae2421dac32c259d20539bd02
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2025-01-03 13:57:30 +0100

    Add data to store and tables

--- src/components/Cars/CarTable.tsx
+++ src/components/Cars/CarTable.tsx
@@ -1,42 +1,74 @@
+import {useEffect, useState} from "react";
+import { load } from '@tauri-apps/plugin-store';
+
 const CarTable = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [carData, setCarData] = useState<any>([]);
+
+    useEffect(() => {
+            if(carData.length > 20){
+                setLoading(false);
+            }
+            else {
+                getCars().then();
+            }
+        },[carData]
+    );
+
+    useEffect(() => {
+        getCars().then();
+    }, []
+    );
+
+    const getCars = async () =>{
+        const store = await load('store.json', { autoSave: false });
+
+        const cars = await store.get<any>('cars');
+        console.log(cars)
+       if (!cars) {
+            const cars = await import('../../data/cars.json');
+            console.log(cars.default);
+            await store.set('cars', cars.default);
+            setCarData(cars);
+            return;
+       }
+       setCarData(cars)
+    }
+
+    if(loading){
+        return (
+            <div>Loading...</div>
+        )
+    }
+
     return (
-        <div className="overflow-x-auto">
-            <table className="table">
-                {/* head */}
-                <thead>
-                <tr>
-                    <th></th>
-                    <th>Name</th>
-                    <th>Job</th>
-                    <th>Favorite Color</th>
-                </tr>
-                </thead>
-                <tbody>
-                {/* row 1 */}
-                <tr>
-                    <th>1</th>
-                    <td>Cy Ganderton</td>
-                    <td>Quality Control Specialist</td>
-                    <td>Blue</td>
-                </tr>
-                {/* row 2 */}
-                <tr>
-                    <th>2</th>
-                    <td>Hart Hagerty</td>
-                    <td>Desktop Support Technician</td>
-                    <td>Purple</td>
-                </tr>
-                {/* row 3 */}
-                <tr>
-                    <th>3</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    )
+    <div className="overflow-x-auto">
+        <table className="table">
+            {/* head */}
+            <thead>
+            <tr>
+                <th></th>
+                <th>Cars Name</th>
+                <th>Cost</th>
+                <th>Owned</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                carData.map((car: any) => (
+                    <tr key={car.id}>
+                        <td></td>
+                        <td>{car.name}</td>
+                        <td>{car.cost}</td>
+                        <td>{car.owned ? <p className="text-xl accent-green-500">Owned</p> : <button className="btn btn-primary">Add to Cart</button>}</td>
+                    </tr>
+                ))
+            }
+            </tbody>
+        </table>
+    </div>
+    );
 }
 
 export default CarTable;
--- src/components/Tracks/TrackTable.tsx
+++ src/components/Tracks/TrackTable.tsx
@@ -1,4 +1,47 @@
+import {useEffect, useState} from "react";
+import { load } from '@tauri-apps/plugin-store';
+
 const TrackTable = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [trackData, setTrackData] = useState<any>([]);
+
+    useEffect(() => {
+            if(trackData.length > 20){
+                setLoading(false);
+            }
+            else {
+                getCars().then();
+            }
+        },[trackData]
+    );
+
+    useEffect(() => {
+            getCars().then();
+        }, []
+    );
+
+    const getCars = async () =>{
+        const store = await load('store.json', { autoSave: false });
+
+        const tracks = await store.get<any>('tracks');
+        console.log(tracks)
+        if (!tracks) {
+            const tracks = await import('../../data/tracks.json');
+            console.log(tracks.default);
+            await store.set('tracks', tracks.default);
+            setTrackData(tracks);
+            return;
+        }
+        setTrackData(tracks)
+    }
+
+    if(loading){
+        return (
+            <div>Loading...</div>
+        )
+    }
+
     return (
         <div className="overflow-x-auto">
             <table className="table">
@@ -6,33 +49,24 @@ const TrackTable = () => {
                 <thead>
                 <tr>
                     <th></th>
-                    <th>Name</th>
-                    <th>Job</th>
-                    <th>Favorite Color</th>
+                    <th>Track Name</th>
+                    <th>Variants</th>
+                    <th>Cost</th>
+                    <th>Owned</th>
                 </tr>
                 </thead>
                 <tbody>
-                {/* row 1 */}
-                <tr>
-                    <th>1</th>
-                    <td>Cy Ganderton</td>
-                    <td>Quality Control Specialist</td>
-                    <td>Blue</td>
-                </tr>
-                {/* row 2 */}
-                <tr>
-                    <th>2</th>
-                    <td>Hart Hagerty</td>
-                    <td>Desktop Support Technician</td>
-                    <td>Purple</td>
-                </tr>
-                {/* row 3 */}
-                <tr>
-                    <th>3</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
-                </tr>
+                {
+                    trackData.map((track: any) => (
+                        <tr key={track.id}>
+                            <td></td>
+                            <td>{track.name}</td>
+                            <td>{track.variants}</td>
+                            <td>{track.cost}</td>
+                            <td>{track.owned ? <p className="text-xl accent-green-500">Owned</p> : <button className="btn btn-primary">Add to Cart</button>}</td>
+                        </tr>
+                    ))
+                }
                 </tbody>
             </table>
         </div>
