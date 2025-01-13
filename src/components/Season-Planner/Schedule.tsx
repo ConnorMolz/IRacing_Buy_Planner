e63bfdcc78b7e4cb7763069a72afb8f24eddd5be
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2025-01-13 12:49:49 +0100

    First implementation

---
+++ src/components/Season-Planner/Schedule.tsx
@@ -0,0 +1,43 @@
+interface plan {
+    plan: any[],
+    tracks: any[]
+}
+
+const Schedule = (plan:plan) => {
+
+    const distanceFormatter = (laps:any, min:any) => {
+        if(laps != null){
+            return laps + " Laps"
+        }
+        return min + " Min"
+    }
+
+    return(
+        <div>
+            <ul className="list bg-base-100 rounded-box shadow-md">
+                {
+                    plan.plan.map((series) =>(
+                        <li className="list-row" key={series.series_id}>
+                            <div tabIndex={0}
+                                 className="collapse collapse-plus border-base-300 bg-base-100 border border-base-300 border">
+                                <div className="collapse-title font-semibold">{series.series_name}</div>
+                                <div className="collapse-content text-sm">
+                                    {series.schedule.map((week:any) =>(
+                                        <div key={week.week}>
+                                            <div>{distanceFormatter(week.race_lap_limit, week.race_time_limit)}</div>
+                                            <div>{week.start_type}</div>
+                                            <div>{week.track}</div>
+                                        </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Schedule;
